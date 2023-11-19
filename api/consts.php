dc43008948341671ec1d0a9e0ccc2a4b4d0b0bee
lbyxiaolizi <65098936+lbyxiaolizi@users.noreply.github.com>
2023-11-19 18:43:12 +0800

    2

--- api/consts.php
+++ api/consts.php
@@ -17,7 +17,7 @@ $promo_code = [
 ];
 
 # 运作模式相关
-$cart_fetch_url = "https://bwh88.net/cart.php"; # 此处不可使用bwh81.net，会出错！建议国外vps使用bandwagonhost.com，国内vps使用bwh88.net
+$cart_fetch_url = "https://bandwagonhost.com/cart.php"; # 此处不可使用bwh81.net，会出错！建议国外vps使用bandwagonhost.com，国内vps使用bwh88.net
 $instant_mode = false; # 即时模式，设为true则访客每访问/刷新一次页面，服务器会向搬瓦工抓取一次数据，显示即时的库存。false为缓存模式。建议使用缓存模式。
 $expire = 60; # 非即时模式/缓存模式下，库存数据的过期时间，单位是秒，例如设为180就是3分钟过期。
 $cart_filename = "cart.txt"; # 非即时模式/缓存模式下，保存库存的文件名，默认即可。务必记得修改php文件所属用户/组为PHP所使用用户组(比如www)，否则上传PHP文件默认为root会报错！
